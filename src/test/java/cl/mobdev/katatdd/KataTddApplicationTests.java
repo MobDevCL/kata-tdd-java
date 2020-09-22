9f891d71240d252c0d2af8077cb6de256165d4ff
xpridex <lopezarcefelipe@gmail.com>
2020-09-22 10:29:19 -0300

    Initial Commit

---
+++ src/test/java/cl/mobdev/katatdd/KataTddApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.mobdev.katatdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataTddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
