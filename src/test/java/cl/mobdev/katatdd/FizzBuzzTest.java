4e9611d6006367b3afe0f56b72707745812287ff
xpridex <lopezarcefelipe@gmail.com>
2020-09-23 09:24:05 -0300

    add the problem statement

---
+++ src/test/java/cl/mobdev/katatdd/FizzBuzzTest.java
@@ -0,0 +1,58 @@
+package cl.mobdev.katatdd;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FizzBuzzTest {
+
+    /*US: Escribe un programa que muestre en pantalla los números del 1 al 100, 
+    sustituyendo los múltiplos de 3 por el palabra “Fizz”, los múltiplos de 5 por “Buzz”
+     y los que son múltiplos tanto de 3 como de 5 deben devolver la palabra “FizzBuzz”.
+     
+     AC:
+     - Si ingresa un 1 devuelve 1
+     - Si ingresa un 2 devuelve 2
+     - Si ingresa un 3 devuelve Fizz
+     - Si ingresa un 5 devuelve Buzz
+     - Si ingresa un 15 devuelve FizzzBuzz
+     - Si ingresa un 90 devuelve ?
+     */
+
+    @Test
+    void give_one_when_getFizzBuzzNumber_then_retun_one() {
+
+    }
+
+    @Test
+    void give_two_when_getFizzBuzzNumber_then_retun_two() {
+
+    }
+
+    @Test
+    void give_three_when_getFizzBuzzNumber_then_retun_Fizz() {
+
+    }
+
+
+    @Test
+    void give_five_when_getFizzBuzzNumber_then_retun_Buzz() {
+
+    }
+
+    @Test
+    void give_fifhteen_when_getFizzBuzzNumber_then_retun_FizzBuzz() {
+
+    }
+
+    @Test
+    void give_nineteen_when_getFizzBuzzNumber_then_retun_FizzBuzz() {
+
+    }
+
+    @Test
+    void test() {
+
+    }
+}
